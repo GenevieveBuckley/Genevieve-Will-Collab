d448fcb961e00e4e0ab5d22cb0a96add2b3916f8
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2022-06-08 18:12:54 -0400

    Test predictions on tiny dataset

--- configs.py
+++ configs.py
@@ -2,6 +2,8 @@ from funlib.geometry import Coordinate, Roi
 from dacapo.experiments.architectures import CNNectomeUNetConfig
 from dacapo.experiments.tasks import DistanceTaskConfig
 
+from skimage.util import img_as_float
+import tifffile
 import torch
 
 input_voxel_size = (8, 8, 8)
@@ -23,6 +25,8 @@ channels = [
     "microtubules",
     "microtubules_out",
 ]
+
+device = 'cpu'
 
 architecture_config = CNNectomeUNetConfig(
     name="CellMapArchitecture",
@@ -58,9 +62,22 @@ task = task_config.task_type(task_config)
 # adding final layers/activations to create the model
 model = task.create_model(architecture)
 
-# load weights from file
-weights_file = "path/to/weights"
-weights = torch.load(weights_file, map_location="cpu")  # map_locations options are "cpu" or "cuda" (this is becuase when pytorch saves weights, it also remembers what device it was on)
+# # load weights from file
+weights_file = "CellmapModelWeights/modelA"
+weights = torch.load(weights_file, map_location=device)  # map_locations options are "cpu" or "cuda" (this is becuase when pytorch saves weights, it also remembers what device it was on)
 
-# initialize model weights
+# # initialize model weights
 model.load_state_dict(weights['model'])
+
+# load data
+filename = "/Volumes/GENEVIEVE/data/janelia/jrc_hela-2_tinycrop.tif"
+image = img_as_float(tifffile.imread(filename))
+print(image.shape)
+
+# select a small region of raw that can be passed through the model:
+image = torch.from_numpy(image).to(device).float()
+image = image.unsqueeze(0).unsqueeze(0)
+
+model.to(device)
+
+prediction = model(image)
