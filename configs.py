a711f34f2e506c40caf2366f4eac4e4c55b18921
pattonw <pattonw@hhmi.org>
2022-06-08 13:22:05 -0400

    Add model initialization and weights loading

--- configs.py
+++ configs.py
@@ -2,14 +2,16 @@ from funlib.geometry import Coordinate, Roi
 from dacapo.experiments.architectures import CNNectomeUNetConfig
 from dacapo.experiments.tasks import DistanceTaskConfig
 
+import torch
+
 channels = [
-    "ecs", # extra cellular space
+    "ecs",  # extra cellular space
     "plasma_membrane",
     "mito",
     "mito_membrane",
     "vesicle",
     "vesicle_membrane",
-    "mvb", # endosomes
+    "mvb",  # endosomes
     "mvb_membrane",
     "er",
     "er_membrane",
@@ -21,8 +23,8 @@ channels = [
 
 architecture_config = CNNectomeUNetConfig(
     name="CellMapArchitecture",
-    input_shape=Coordinate(216, 216, 216), # can be changed
-    eval_shape_increase=Coordinate(72, 72, 72), # can be changed
+    input_shape=Coordinate(216, 216, 216),  # can be changed
+    eval_shape_increase=Coordinate(72, 72, 72),  # can be changed
     fmaps_in=1,
     num_fmaps=12,
     fmaps_out=72,
@@ -36,10 +38,26 @@ task_config = DistanceTaskConfig(
     name="DistancePrediction",
     # important
     channels=channels,
-    scale_factor=50, # target = tanh(distance / scale)
+    scale_factor=50,  # target = tanh(distance / scale)
     # training
     mask_distances=True,
     # evaluation
     clip_distance=50,
     tol_distance=10,
 )
+
+# create backbone from config
+architecture = architecture_config.architecture_type(architecture_config)
+
+# initialize task from config
+task = task_config.task_type(task_config)
+
+# adding final layers/activations to create the model
+model = task.create_model(architecture)
+
+# load weights from file
+weights_file = "path/to/weights"
+weights = torch.load(weights_file, map_location="cpu")  # map_locations options are "cpu" or "cuda" (this is becuase when pytorch saves weights, it also remembers what device it was on)
+
+# initialize model weights
+model.load_state_dict(weights['model'])
